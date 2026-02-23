3ea38105701d26f93c2be7b3d3dcde29c982052a
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-23 19:06:33 +0100

    Made the checkUser which sends a fetch to the backend with the token in params. It then gets back the user found from the id inside the cookie and saves it in res.locals.user

--- app.js
+++ app.js
@@ -4,36 +4,42 @@ const cors = require("cors");
 
 const path = require("path");
 
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
 const app = express();
 
-const default_routes = require("./routes/default_routes")
+const { checkUser } = require("./middleware/authMiddleware");
 
-const auth_routes = require("./routes/auth_routes")
+const default_routes = require("./routes/default_routes");
+
+const auth_routes = require("./routes/auth_routes");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.HOST,
-    methods: ["GET","POST", "DELETE","PUT"],
-    allowedHeaders: ["Content-Type","Authorization"]
-}))
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-app.use(default_routes)
+app.use(checkUser);
 
-app.use(auth_routes)
+app.use(default_routes);
 
-app.listen(process.env.PORT,"0.0.0.0", async()=>{
-console.log("Server is running on port:", process.env.PORT)
+app.use(auth_routes);
+
+app.listen(process.env.PORT, "0.0.0.0", async () => {
+  console.log("Server is running on port:", process.env.PORT);
 });
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@
 const authenticate =async(req,res, next)=>{
     const token = req.cookies.jwt
         if(token){
-            const response = await fetch(`http://localhost:6001/verifyJwt/${token}`,{
+            const response = await fetch(`http://10.12.14.239:6001/verifyJwt/${token}`,{
                 method: "GET",
                 headers: {"Content-Type": "application/json"}
             })
@@ -17,4 +17,26 @@ const authenticate =async(req,res, next)=>{
             res.redirect("/loggInn")
         }
 }
-module.exports = {authenticate}
+
+const checkUser = async(req,res, next)=>{
+    const token = req.cookies.jwt
+        if(token){
+            const response = await fetch(`http://10.12.14.239:6001/sendBackUser/${token}`,{
+                method: "GET",
+                headers: {"Content-Type": "application/json"}
+            })
+
+            const data = await response.json()
+            if(data.user){
+                res.locals.user = data.user
+                next()
+            }else{
+            res.locals.user = null
+                next()
+            }
+        }else{
+           res.locals.user = null
+            next()
+        }
+}
+module.exports = {authenticate, checkUser}
