cd383cab0eaaf90dc833e38aea8f4326aad2b826
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-23 18:49:54 +0100

    Fixed the issue with middleware not redirecting correctly after it fails to verify

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,12 +2,12 @@
 const authenticate =async(req,res, next)=>{
     const token = req.cookies.jwt
         if(token){
-            const res = await fetch(`http://10.12.14.239:6001/verifyJwt/${token}`,{
+            const response = await fetch(`http://localhost:6001/verifyJwt/${token}`,{
                 method: "GET",
                 headers: {"Content-Type": "application/json"}
             })
 
-            const data = await res.json()
+            const data = await response.json()
             if(data.success){
                 next()
             }else{
