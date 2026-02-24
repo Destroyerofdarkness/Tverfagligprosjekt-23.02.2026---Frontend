09f0d707994c1bcbd329c0f5f5c5a66f880d2047
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-24 18:45:17 +0100

    Made the fetch to delete the specific comment when the button is clicked

---
+++ public/script/fetch_delete_comment.js
@@ -0,0 +1,15 @@
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("deleteComment")) {
+    const commentId = e.target.dataset.id;
+    await fetch("http://10.12.14.239:6001/comments/delete", {
+      method: "DELETE",
+      body: JSON.stringify({commentId}),
+        headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+  if(data.success){
+    window.location.href = "/"
+  }
+  }
+});
