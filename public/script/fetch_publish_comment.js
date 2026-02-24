3163203d1fe5f613fc51c35566f7173a63106d59
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-24 16:55:19 +0100

    Made the comment publishing and styled the review page more for it

---
+++ public/script/fetch_publish_comment.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+
+const commentError=document.querySelector(".error.comment")
+
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();   
+    const content = form.content.value
+    const user  = form.user.value
+    const connection = form.connection.value
+    
+  const res = await fetch("http://10.12.14.239:6001/comment/publish", {
+    method: "POST",
+    body: JSON.stringify({content, user, connection}),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  if(data.success){
+    window.location.reload()
+  }
+  if(data.errors){
+    contentError.innerHTML = data.errors.content
+  }
+});
