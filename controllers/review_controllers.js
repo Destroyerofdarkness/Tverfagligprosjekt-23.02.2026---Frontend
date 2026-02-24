21e45eabaa96813529dd3150b3d809439bb570da
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-24 13:12:10 +0100

    Made the review page and fixed some css issues

--- controllers/review_controllers.js
+++ controllers/review_controllers.js
@@ -1,3 +1,5 @@
+const getData = require("../handlers/getDataHandler");
+
 const render_review_publish = (req, res) => {
   try {
     res.render("publish", { title: "Publiser" });
@@ -7,6 +9,23 @@ const render_review_publish = (req, res) => {
   }
 };
 
+const render_review_page = async(req,res, next)=>{
+const reviewId = req.params.id
+try {
+  const {review,err} = await getData(`/review/${reviewId}`)
+  if(err){
+    throw Error(err)
+  }
+  console.log("Rendered review:", review);
+  res.render("review",{title:"Vurdering", review})
+} catch (err) {
+  console.log(err)
+  res.status(404).send(err)
+  next()
+}
+}
+
 module.exports = {
   render_review_publish,
+  render_review_page
 };
--- routes/review_routes.js
+++ routes/review_routes.js
@@ -6,4 +6,6 @@ const { authenticate } = require("../middleware/authMiddleware");
 
 router.get("/publiser", authenticate, controller.render_review_publish);
 
+router.get("/:id",controller.render_review_page)
+
 module.exports = router;
