266b5507f560b429ecc1c0c9b90b0f3f78072bb7
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-25 09:19:42 +0100

    Made the form for reporting the review and made a fetch to the backend

---
+++ public/script/fetch_publish_report.js
@@ -0,0 +1,20 @@
+const report = document.querySelector("#report");
+
+
+report.addEventListener("submit", async (e) => {
+  e.preventDefault();   
+    const content = form.content.value
+    const user  = form.user.value
+    const connection = form.connection.value
+    
+  const res = await fetch("http://10.12.14.239:6001/report/publish", {
+    method: "POST",
+    body: JSON.stringify({content, user, connection}),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  if(data.success){
+    window.location.reload()
+  }
+});
