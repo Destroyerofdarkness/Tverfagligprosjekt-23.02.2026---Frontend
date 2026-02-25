fe385851cd29e9d495c9ad733eb22fd7fbde640f
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-25 12:29:50 +0100

    Small fixes

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,11 +1,13 @@
 
 const checkAdmin = (req,res,next)=>{
     const user = res.locals.user
+    if(user){
     if(user.role === "admin"){
         next()
     }else{
         res.redirect("/")
     }
+}
 }
 
 module.exports = {checkAdmin}
