b3ded474bbd0c0e6b3f9fb974876703b7507106a
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-25 11:56:37 +0100

    Made the FAQ page and also added footer where you can navigate to FAQ page and also Mail the administrator

--- controllers/default_controllers.js
+++ controllers/default_controllers.js
@@ -1,36 +1,45 @@
-const getData = require("../handlers/getDataHandler")
+const getData = require("../handlers/getDataHandler");
 
-const render_homepage = async(req,res)=>{
-    try{
-        const {reviews} = await getData("/review/sendOutReviews")
-        console.log(reviews)
-        res.render("index", {title: "Hjemmeside", reviews})
-    }catch(err){
-        res.status(500).send("Error:", err)
-    }
-}
+const render_homepage = async (req, res) => {
+  try {
+    const { reviews } = await getData("/review/sendOutReviews");
+    console.log(reviews);
+    res.render("index", { title: "Hjemmeside", reviews });
+  } catch (err) {
+    res.status(500).send("Error:", err);
+  }
+};
 
-const render_404 = async(req,res)=>{
-    try {
-        res.render("404",{title: "Siden Finnes Ikke!!"})
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err)
-    }
-}
+const render_404 = async (req, res) => {
+  try {
+    res.render("404", { title: "Siden Finnes Ikke!!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
 
-const render_admin_page = async(req,res)=>{
-    try {
-        const {reports} = await getData("/report/send_out")
-        res.render("admin", {title: "Raporteringer", reports})
-    } catch (err) {
-        res.status(500).send(err);
-    }
-}
+const render_admin_page = async (req, res) => {
+  try {
+    const { reports } = await getData("/report/send_out");
+    res.render("admin", { title: "Raporteringer", reports });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 
+const render_FAQ = async (req, res) => {
+  try {
+    res.render("FAQ", { title: "FAQ" });
+  } catch (err) {
+    console.log(err);
+    res.status(200).send(err);
+  }
+};
 
 module.exports = {
-    render_homepage,
-    render_404,
-    render_admin_page
-}
+  render_homepage,
+  render_404,
+  render_admin_page,
+  render_FAQ,
+};
--- routes/default_routes.js
+++ routes/default_routes.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
 
-const {checkAdmin} = require("../middleware/authorization")
+const { checkAdmin } = require("../middleware/authorization");
 
 const controller = require("../controllers/default_controllers");
 
+router.get("/", controller.render_homepage);
 
+router.get("/admin", checkAdmin, controller.render_admin_page);
 
-router.get("/" ,controller.render_homepage);
+router.get("/FAQ", controller.render_FAQ);
 
-router.get("/admin",checkAdmin,controller.render_admin_page)
-
-router.use(controller.render_404)
-
+router.use(controller.render_404);
 
 module.exports = router;
